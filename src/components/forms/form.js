3204a78b68b24806b173e16c1cf14108ac60a67e
angelod1as <angelo@angelodias.com.br>
2019-07-28 20:05:16 -0300

    testing xhr

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 const config = {
   script:
@@ -8,19 +8,8 @@ const config = {
   // email: '',
 }
 
-const Start = () => (
-  <div>
-    <title>Contact Form Example</title>
-    <h2>Contact Us!</h2>
-    <aside>
-      <p>We would love to hear from you!</p>
-      <p>Please use the Contact Form to send us a message.</p>
-    </aside>
-  </div>
-)
-
 const Thanks = () => (
-  <div className="thankyou_message" style={{ display: 'none' }}>
+  <div className="thankyou_message">
     <h2>Thanks for contacting us! We will get back to you soon!</h2>
   </div>
 )
@@ -35,6 +24,8 @@ class Form extends Component {
         email: '',
         honeypot: '',
       },
+      sent: false,
+      clicked: false,
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -42,21 +33,38 @@ class Form extends Component {
   }
 
   handleClick = () => {
-    console.log('validating')
     const { form } = this.state
+
+    // loading
+    this.setState({ clicked: true })
+
+    // sent
+    const setSent = () => {
+      this.setState({ sent: true })
+    }
+
+    // checking if bot
     const { honeypot } = form
     if (honeypot) {
       return false
     }
-    axios.post(config.script, {
-      withCredentials: true,
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-      data: form,
-    })
+
+    // sending through xhr as axios won't work (CORS)
+    const xhr = new XMLHttpRequest()
+    xhr.open('POST', config.script)
+    // xhr.withCredentials = true
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    xhr.onreadystatechange = function xhrGo() {
+      setSent()
+    }
+    const encoded = Object.keys(form)
+      .map(k => {
+        return `${encodeURIComponent(k)}=${encodeURIComponent(form[k])}`
+      })
+      .join('&')
+
+    // finally sending
+    xhr.send(encoded)
     return true
   }
 
@@ -70,78 +78,90 @@ class Form extends Component {
   render() {
     const {
       form: { name, message, email, honeypot },
+      sent,
+      clicked,
     } = this.state
 
     return (
       <>
-        <Start />
-        <form
-        // method="POST"
-        // action={config.script} // change this url
-        >
-          <div>
-            <fieldset>
-              <label htmlFor="name">
-                Name:
-                <input
-                  onChange={this.handleChange}
-                  id="name"
-                  key="name"
-                  name="name"
-                  placeholder="What your Mom calls you"
-                  value={name}
-                />
-              </label>
-            </fieldset>
-            <fieldset>
-              <label htmlFor="message">
-                Message:
-                <textarea
-                  onChange={this.handleChange}
-                  id="message"
-                  key="message"
-                  name="message"
-                  rows={10}
-                  placeholder="Tell us what's on your mind..."
-                  value={message}
-                />
-              </label>
-            </fieldset>
-            <fieldset>
-              <label htmlFor="email">
-                Your Email Address:
-                <input
-                  onChange={this.handleChange}
-                  id="email"
-                  key="email"
-                  name="email"
-                  type="email"
-                  required
-                  placeholder="your.name@email.com"
-                  value={email}
-                />
-              </label>
-            </fieldset>
-            <fieldset className="honeypot-field">
-              <label htmlFor="honeypot">
-                To help avoid spam, utilize a Honeypot technique with a hidden text field; must be
-                empty to submit the form! Otherwise, we assume the user is a spam bot.
-                <input
-                  onChange={this.handleChange}
-                  id="honeypot"
-                  key="honeypot"
-                  type="text"
-                  name="honeypot"
-                  value={honeypot}
-                />
-              </label>
-            </fieldset>
-            <button type="button" onClick={() => this.handleClick()}>
-              Send
-            </button>
-          </div>
+        {sent ? (
           <Thanks />
-        </form>
+        ) : (
+          <>
+            <form
+            // method="POST"
+            // action={config.script} // change this url
+            >
+              <div>
+                <fieldset>
+                  <label htmlFor="name">
+                    Name:
+                    <input
+                      onChange={this.handleChange}
+                      id="name"
+                      key="name"
+                      name="name"
+                      placeholder="What your Mom calls you"
+                      value={name}
+                    />
+                  </label>
+                </fieldset>
+                <fieldset>
+                  <label htmlFor="message">
+                    Message:
+                    <textarea
+                      onChange={this.handleChange}
+                      id="message"
+                      key="message"
+                      name="message"
+                      rows={10}
+                      placeholder="Tell us what's on your mind..."
+                      value={message}
+                    />
+                  </label>
+                </fieldset>
+                <fieldset>
+                  <label htmlFor="email">
+                    Your Email Address:
+                    <input
+                      onChange={this.handleChange}
+                      id="email"
+                      key="email"
+                      name="email"
+                      type="email"
+                      required
+                      placeholder="your.name@email.com"
+                      value={email}
+                    />
+                  </label>
+                </fieldset>
+                <fieldset className="honeypot-field" style={{ display: 'none' }}>
+                  <label htmlFor="honeypot">
+                    {/* To help avoid spam, utilize a Honeypot technique with a hidden text field; must
+                    be empty to submit the form! Otherwise, we assume the user is a spam bot. */}
+                    <input
+                      onChange={this.handleChange}
+                      id="honeypot"
+                      key="honeypot"
+                      type="text"
+                      name="honeypot"
+                      value={honeypot}
+                    />
+                  </label>
+                </fieldset>
+                {clicked ? (
+                  <button type="button" disabled>
+                    Loading
+                  </button>
+                ) : (
+                  <button type="button" onClick={() => this.handleClick()}>
+                    Send
+                  </button>
+                )}
+              </div>
+            </form>
+          </>
+        )}
       </>
     )
   }
